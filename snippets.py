21f857d775f13b7a0a1ac4771f998ced7c169cfc
mae-tramonto <octo.sammy@gmail.com>
2016-11-11 15:35:24 +0000

    Added main() function and first parser

--- snippets.py
+++ snippets.py
@@ -1,4 +1,5 @@
 import logging
+import argparse
 
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 
@@ -9,3 +10,14 @@ def put(name, snippet):
 def get(name):
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
     return " "
+    
+def main():
+    """main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description = "Store and retrieve snippets of text")
+    arguments = parser.parse_args()
+    
+    
+    
+if __name__ == "__main__":
+    main()
